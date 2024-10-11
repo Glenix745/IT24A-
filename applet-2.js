17c69803605c4808f714eae02510fa8d57fe608a
Glenix745 <ocmayonglen83@gmail.com>
2024-10-12 01:17:55 +0800

    Added class LeafletMap and constructor

---
+++ applet-2.js
@@ -0,0 +1,15 @@
+class LeafletMap {
+
+    constructor(containerId, center, zoom) {
+        this.map = L.map(containerId).setView(center, zoom);
+        this.initTileLayer();
+    }
+
+   
+  
+   
+
+
+
+
+
