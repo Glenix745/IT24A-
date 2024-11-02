43ab9eab7528732c7fa178097eb7a5171fa50faf
Glenix745 <ocmayonglen83@gmail.com>
2024-11-02 17:04:52 +0800

    Added updatetask task text

--- applet-6.js
+++ applet-6.js
@@ -46,4 +46,9 @@ class TodoList {
         buttons.forEach(button => button.disabled = true);
     }
 
-    
+    updateTask(taskText) {
+        this.todoList.children[this.editingIndex].querySelector('.task-text').textContent = taskText;
+        this.resetEditing();
+    }
+
+   
